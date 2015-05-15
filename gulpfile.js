5d9eaa62dc1f38ba8475bccc41e06b030e209b0a
wzaghal <wisamzaghal@gmail.com>
2015-05-14 23:26:31 -0400

    setup build tasks and testing environment

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var prettify = require('gulp-jsbeautifier');
+var eslint = require('gulp-eslint');
+
+var jsFiles = ['*.js', 'tests/**/*.test.js'];
+
+
+gulp.task('mocha-ci', function () {
+  return gulp.src(jsFiles[1], {
+      read: false
+    })
+    .pipe(mocha({
+      reporter: 'min'
+    }));
+});
+
+gulp.task('beautify', function () {
+  gulp.src(jsFiles, {
+      base: '.'
+    })
+    .pipe(prettify({
+      config: '.jsbeautifyrc',
+      mode: 'VERIFY_AND_WRITE'
+    }))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('beautify:build', function () {
+  gulp.src(jsFiles)
+    .pipe(prettify({
+      config: '.jsbeautifyrc',
+      mode: 'VERIFY_ONLY'
+    }));
+});
+
+
+/**
+ * JavaScript Linting Task
+ */
+gulp.task('lint', function () {
+  return gulp.src(jsFiles)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+/**
+ * JavaScript Build Lint Task
+ */
+gulp.task('lint:build', function () {
+  return gulp.src(jsFiles)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('mocha-watch', function () {
+  gulp.watch(jsFiles, ['mocha-ci']);
+});
+
+gulp.task('default', ['beautify', 'mocha-watch']);
+gulp.task('test', ['beautify:build', 'lint:build', 'mocha-ci']);
---
+++ tests/dummy.test.js
@@ -0,0 +1,13 @@
+/*eslint-disable no-unused-expressions*/
+
+'use strict';
+
+var expect = require('chai').expect;
+describe('dummy', function () {
+
+  it('should be a dummy test file for now', function () {
+    expect(true).to.be.true;
+
+  });
+
+});
