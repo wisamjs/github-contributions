abe3e1f80c26a2e05508d1bfee33af79ecbd709e
wzaghal <wisamzaghal@gmail.com>
2015-05-20 23:37:08 -0400

    add e2e tests

--- tests/e2e-tests/github.test.js
+++ tests/e2e-tests/github.test.js
@@ -1,30 +1,48 @@
 var chai = require('chai');
-var assert = chai.assert;
+var expect = chai.expect;
 var webdriverio = require('webdriverio');
 
-describe('my webdriverio tests', function () {
+describe('Github.com contributions', function () {
 
   this.timeout(99999999);
   var client = {};
-
+  var htmlResponse;
+  var errResponse;
   before(function (done) {
     client = webdriverio.remote({
       desiredCapabilities: {
         browserName: 'phantomjs'
       }
     });
-    client.init(done);
-  });
-
-  it('Github test', function (done) {
-    client
-      .url('https://github.com/')
-      .getTitle(function (err, title) {
-        assert.equal(undefined, err);
-        assert.strictEqual(title,
-          'GitHub · Build software better, together.');
+    client.init()
+      .url('https://github.com/octocat')
+      .getHTML('.contrib-number', false, function (err, html) {
+        errResponse = err;
+        htmlResponse = html;
       })
       .call(done);
+  });
+
+  it('should return html', function () {
+    expect(errResponse).to.eql(null);
+    expect(htmlResponse).to.not.eql(null);
+  });
+
+
+  it('should have 3 contributions', function () {
+    expect(htmlResponse.length).to.eql(3);
+  });
+
+  it('should show last year\'s contributions', function () {
+    expect(htmlResponse[0]).to.eql('2 total');
+  });
+
+  it('should show longest streak', function () {
+    expect(htmlResponse[1]).to.eql('2 days');
+  });
+
+  it('should show current streak', function () {
+    expect(htmlResponse[2]).to.eql('0 days');
   });
 
   after(function (done) {
